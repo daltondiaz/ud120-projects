a89abed864386f0710eaa1c92ebf849dfdc898d3
daltondiz <dalton.dias@ymail.com>
2018-06-30 13:17:10 -0300

    Quizzes: SVM Author ID Accuracy and Timing, Smaller Trainning Set and Speed-Accurary Tradeoff

--- svm/svm_author_id.py
+++ svm/svm_author_id.py
@@ -12,15 +12,31 @@ import sys
 from time import time
 sys.path.append("../tools/")
 from email_preprocess import preprocess
-
+from sklearn.svm import SVC
+from sklearn.metrics import accuracy_score
 
 ### features_train and features_test are the features for the training
 ### and testing datasets, respectively
 ### labels_train and labels_test are the corresponding item labels
 features_train, features_test, labels_train, labels_test = preprocess()
 
+### These lines effectively slice the training dataset down to 1% 
+### of its original size, tossing out 99% of the training data.
+features_train = features_train[:len(features_train)/100] 
+labels_train = labels_train[:len(labels_train)/100] 
 
 
+print "Begin trainning:"
+t0 = time()
+clf = SVC(kernel='linear')
+clf.fit(features_train, labels_train)
+print "training time:", round(time()-t0, 3), "s"
+
+t1 = time()
+pred = clf.predict(features_test)
+print "predict time:", round(time()-t1, 3), "s"
+
+print "Accuracy: ", accuracy_score(pred, labels_test)
 
 #########################################################
 ### your code goes here ###
