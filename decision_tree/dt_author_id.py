2400278dd74199333d705ba4aaf99f603cf0cd8e
daltondiz <dalton.dias@ymail.com>
2018-07-17 23:08:37 -0300

    [Decision Tree] Quiz: Your First Email DT: Accuracy

--- decision_tree/dt_author_id.py
+++ decision_tree/dt_author_id.py
@@ -12,6 +12,8 @@ import sys
 from time import time
 sys.path.append("../tools/")
 from email_preprocess import preprocess
+from sklearn import tree
+from sklearn.metrics import accuracy_score
 
 
 ### features_train and features_test are the features for the training
@@ -19,8 +21,11 @@ from email_preprocess import preprocess
 ### labels_train and labels_test are the corresponding item labels
 features_train, features_test, labels_train, labels_test = preprocess()
 
+clf = tree.DecisionTreeClassifier(min_samples_split=40)
+clf = clf.fit(features_train, labels_train)
+pred = clf.predict(features_test)
 
-
+print "Accuracy: ", accuracy_score(pred, labels_test)
 
 #########################################################
 ### your code goes here ###
